70459b4072da36083f4c8c607b0412825e3f31ef
Dmitry Bychko <virus_dmk@mail.ru>
2019-03-19 14:43:47 +0300

    feat/: task done

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,5 +1,6 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
+// module.exports = function makeExchange(currency) {
+function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
     const H = 50;
@@ -7,7 +8,6 @@ module.exports = function makeExchange(currency) {
     const D = 10;
     const N = 5;
     const P = 1;
-
     let ex = {
         H: 0,
         Q: 0,
@@ -16,6 +16,9 @@ module.exports = function makeExchange(currency) {
         P: 0
     }
 
+    function dimension(velue, a, b){
+        
+    }
     if(currency<=0) return {};
     if(currency>10000) return  {error: "You are rich, my friend! We don't have so much coins for exchange"};
     else{
@@ -23,15 +26,17 @@ module.exports = function makeExchange(currency) {
     ex.Q = Math.floor((currency-H*ex.H)/25);
     ex.D = Math.floor((currency-H*ex.H-Q*ex.Q)/10);
     ex.N = Math.floor((currency-H*ex.H-Q*ex.Q-D*ex.D)/5);
-    ex.P = currency - H * ex.H - Q * ex.Q - D * ex.D - N * ex.N;
+    ex.P = currency-H*ex.H-Q*ex.Q-D*ex.D-N*ex.N;
     }
 
     let outOb = {};
 
-    for(let key in ex){
-        if( ex[key]>0)
-            outOb[key]=ex[key];
+    for(key in ex){
+        if(ex[key]>0)
+            outOb.key=ex.key;
     }
     return outOb;
 
 }
+
+makeExchange(98);
